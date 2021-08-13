9a8c1b5a72692027a4e9a977832d145098fc266f
Petteri Parviainen <daolamis@gmail.com>
2021-08-13 13:18:40 +0300

    implemented exercise 3.19

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URL;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -10,9 +11,10 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   });
 
 const personSchema = mongoose.Schema({
-  name: String,
+  name: { type: String, unique: true },
   number: String
 });
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
